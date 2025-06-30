72b80f83f338fc900d894930d4342f268a669333
saeedhumai <sanwar@humai-ai.com>
2025-07-01 01:48:48 +0400

    learning is input and output should be in graphstate

--- graph.py
+++ graph.py
@@ -10,35 +10,34 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class StatA(TypedDict):
-    cal_add : str 
-    cal_sub : str
-    cal_mul : str
-    cal_div : str
+    cal_add : int
+    cal_sub : int
+    cal_mul : int
+    cal_div : int
+    input : str
 
 
 def process_inpute(state: StatA) -> StatA:
     value = state["input"]
     if value == "add":
-        x = input (f"enter the first number")
-        y = input (f"enter the second number")
-        result = x + y
+        x = input (f"enter the first number : ")
+        y = input (f"enter the second number : ")
+        result = print(f"the result of {x} + {y} is { int(x) + int(y) }")
         return {"cal_add": result}
     
     elif value == "sub":
-        x = input (f"enter the first number")
-        y = input (f"enter the second number")
-        result = x - y
+        x = input (f"enter the first number : ")
+        y = input (f"enter the second number : ")
+        result = print(f"the result of {x} - {y} is { int(x) - int(y) }")
         return {"cal_sub": result}
     elif value == "mul":
-        x = input (f"enter the first number")
-        y = input (f"enter the second number")
-        result = x * y
-
+        x = input (f"enter the first number : ")
+        y = input (f"enter the second number : ")
+        result = print(f"the result of {x} * {y} is { int(x) * int(y) }")
 
 
 
 workflow = StateGraph(StatA)
-
 workflow.add_node("process_inpute", process_inpute)
 workflow.add_edge(START, "process_inpute")
 workflow.add_edge("process_inpute", END)
